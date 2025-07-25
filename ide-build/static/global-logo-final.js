d6070b64cc4efbedd10f9aa94acdecc4a9a0602e
Michael Kraft <michaelkraft@Michaels-MacBook-Air.local>
2025-07-25 11:35:31 -0600

    Move logo up by 50px using relative positioning

--- ide-build/static/global-logo-final.js
+++ ide-build/static/global-logo-final.js
@@ -36,6 +36,10 @@
                 el.style.setProperty('max-height', '140px', 'important');
                 el.style.setProperty('object-fit', 'contain', 'important');
                 
+                // Move the logo up
+                el.style.setProperty('position', 'relative', 'important');
+                el.style.setProperty('top', '-50px', 'important');
+                
                 // If it's not our custom logo, replace the src
                 if (el.tagName === 'IMG' && !el.src.includes('coder1-logo.svg')) {
                     el.src = './static/coder1-logo.svg';
@@ -79,6 +83,8 @@
                     max-width: 140px !important;
                     max-height: 140px !important;
                     object-fit: contain !important;
+                    position: relative !important;
+                    top: -50px !important;
                 }
                 
                 /* Style the parent containers too */
